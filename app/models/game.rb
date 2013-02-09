78b9698861e4dc155b365456d500b1be3ccfa723
Georgia A <and.georgia.m@gmail.com>
2013-02-08 17:27:10 -0800

    reverted changes to have an 'original' branch to apply changes for pull requests

--- app/models/game.rb
+++ app/models/game.rb
@@ -46,26 +46,28 @@ class Game < ActiveRecord::Base
   # Gets the next player
   # @return [String] The next player
   def next_player
-    self.current_player == 'red' ? 'blue' : 'red'
+    # TODO
+
   end
 
   # setNextPlayer sets the next player
   #
   # @return [String] The new player
   def set_next_player
-    self.current_player = next_player
+    # TODO
+
   end
 
   # returns the piece at the coordinates
   def board_position(coords)
-    raise ArgumentError, 'those are invalid coordinates' unless coords_valid?(coords)
-    board[coords.first][coords.last]
+    # TODO
+
   end
 
   # checks that the given coordinates are within the valid range
   def coords_valid?(coords)
-    raise ArgumentError, 'what dimension are you playing in? wrong # of coords' unless coords.size == 2
-    row_in_bounds?(coords.last) and column_in_bounds?(coords.first)
+    # TODO
+
   end
 
   # MakeMove takes a column and player and updates the board to reflect the
@@ -74,82 +76,25 @@ class Game < ActiveRecord::Base
   # @param column [Integer]
   # @param player [String] either 'red' or 'blue'
   def make_move(column, player)
-    raise ArgumentError, 'column out of bounds' unless column_in_bounds?(column)
-    raise ArgumentError, 'column full' unless board[column].size < NUM_ROWS
-    raise ArgumentError, 'invalid player' unless %w(blue red).include? player
+    # TODO
 
-    board[column] << player
-    self.status = check_for_winner
-    set_next_player
   end
 
   # Checks if there is a winner and returns the player if it exists
   #
-  # @return [String] 'red', 'blue', 'tie', or 'in_progress'
+  # @return [String] 'red', 'blue', 'tie', 'draw', or 'in_progress'
   def check_for_winner
+    # TODO
 
-    each_board_position do |coord|
-      if check_column(coord) or
-            check_row(coord) or
-            check_left_diagonal(coord) or
-            check_right_diagonal(coord)
-        return board_position(coord)
-      end
-    end
-
-    return 'draw' if board_is_full
-    'in_progress'
   end
+
+
 
   #############################################################################
 
   private
 
-  def row_in_bounds?(row)
-    row >= 0 and row < NUM_ROWS
-  end
+  # NOTE: Put all helper-methods here!
 
-  def column_in_bounds?(column)
-    column >= 0 and column < NUM_COLUMNS
-  end
 
-  def board_is_full
-    (0..NUM_COLUMNS-1).each do |i|
-      return false if board_position([i, NUM_ROWS - 1]).nil?
-    end
-    true
-  end
-
-  def check_column(coords)
-    check_direction(coords) do |coord, i|
-      vertical(coord, i)
-    end
-  end
-
-  def check_right_diagonal(coords)
-    check_direction(coords) do |coord, i|
-      diagonal_right(coord, i)
-    end
-  end
-
-  def check_left_diagonal(coords)
-    check_direction(coords) do |coord, i|
-      diagonal_left(coord, i)
-    end
-  end
-
-  def check_row(coords)
-    check_direction(coords) do |coord, i|
-      horizontal(coord, i)
-    end
-  end
-
-  def check_direction(coords)
-    start_pos = board_position coords
-    return false if start_pos.nil?
-
-    (1..3).all? {|i|
-      coords_valid?(yield(coords, i)) and board_position(yield(coords, i)) == start_pos
-    }
-  end
 end
